76857d19ef04a8e470e4cee7f0cce3b77d979e58
BuiTrongThanh03 <128080669+BuiTrongThanh03@users.noreply.github.com>
2024-07-23 10:11:03 +0700

    Add files via upload

---
+++ Sneaker/Sneaker/src/test/java/E_commerce/Sneaker/SneakerApplicationTests.java
@@ -0,0 +1,13 @@
+package E_commerce.Sneaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SneakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
