ff5f4bd59802037afd0aa0ebfe9c41d1bad6f933
BuiTrongThanh03 <128080669+BuiTrongThanh03@users.noreply.github.com>
2024-07-27 19:40:28 +0700

    add homepage

--- Sneaker/Fontend-Sneaker/index.js
+++ Sneaker/Fontend-Sneaker/index.js
@@ -2,11 +2,11 @@ const express = require('express')
 const app = express()
 const path = require('path');
 const port = 3000
-
+const bodyParser = require('body-parser');
 
 app.use(express.static(path.join(__dirname, 'views')));
 app.use(express.urlencoded({ extended: true }));
-
+app.use(bodyParser.json());
 
 const loginRoutes = require('./routes/login');
 const signupRoutes = require('./routes/signup');
@@ -17,4 +17,4 @@ app.use('/signup', signupRoutes);
 
 
 
-app.listen(port, () => console.log(`http://localhost: ${port}`))
+app.listen(port, () => console.log(`http://localhost:${port}`))
