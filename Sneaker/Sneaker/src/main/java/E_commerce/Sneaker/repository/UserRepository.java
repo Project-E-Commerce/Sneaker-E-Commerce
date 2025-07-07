ad553b3d9bb88ea7865b0c3976dc9537c287d336
Tung Nguyen X. <xuntgn12@gmail.com>
2025-07-07 21:23:33 +0700

    1st commit July 7th 2025

--- Sneaker/Sneaker/src/main/java/E_commerce/Sneaker/repository/UserRepository.java
+++ Sneaker/Sneaker/src/main/java/E_commerce/Sneaker/repository/UserRepository.java
@@ -2,13 +2,14 @@ package E_commerce.Sneaker.repository;
 
 
 import E_commerce.Sneaker.model.User.User;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long>, PagingAndSortingRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>{
     Optional<User> findByUsername(String username);
     Optional<User> findByUserId (Long id);
     boolean existsByUsername(String username);
+
 }
